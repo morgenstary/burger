5b373943af4b8cc4cc5de4fc33d684f40819348e
Morgen Stary <morgenstary@North-Pole.local>
2018-11-08 21:01:23 -0800

    commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Avocado Pepper Jack');
+INSERT INTO burgers (burger_name) VALUES ('Blue Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Red Hot Chili Cheese Burger');
